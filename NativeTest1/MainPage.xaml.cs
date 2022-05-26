c6e0debbcf96d58c204277750d704cc9dcd57156
Angel Lavid <angel_lavid@epam.com>
2022-05-26 13:25:53 -0500

    Add project files.

---
+++ NativeTest1/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NativeTest1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            webtestname.ScriptNotify += Webtestname_ScriptNotify;
+
+        }
+
+        private async void Webtestname_ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            switch (e.Value.ToLower())
+            {
+                case "loading":
+                case "getdevices":
+                    // get the list of the devices and send them back
+                    var deviceList = await GetAllConnectedCameras();
+                    // fake some camera devices
+                    deviceList.Add("fake cam 1");
+                    deviceList.Add("fake cam 2");
+                    deviceList.Add("fake cam 3");
+                    await webtestname.InvokeScriptAsync("deviceList", new List<string> { JsonConvert.SerializeObject(deviceList) });
+                    break;
+                case "activate_camera":
+                    // Open camera device
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public async Task<List<string>> GetAllConnectedCameras()
+        {
+            var cameraNames = new List<string>();
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            var reader = await DeviceInformation.FindAllAsync(DeviceClass.ImageScanner);
+            allVideoDevices.ToList().ForEach(d=> {
+                cameraNames.Add(d.Name);
+            });
+
+            return cameraNames;
+        }
+
+    }
+}
---
+++ runtimeCompTest1/KeyHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+
+namespace runtimeCompTest1
+{
+    [AllowForWeb]
+    public sealed class KeyHandler
+    {    
+        public void setKeyCombination(int keyPress)
+        {
+            Debug.WriteLine("Called from WebView! {0}", keyPress);
+        }
+    }
+}
